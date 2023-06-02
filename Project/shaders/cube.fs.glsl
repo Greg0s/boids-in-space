399c1ac694ec8411270b0ca0aeec90b3c3789685
Greg0s <gregoire.tinnes@outlook.fr>
2023-06-02 16:35:34 +0200

    rm light

--- Project/shaders/cube.fs.glsl
+++ Project/shaders/cube.fs.glsl
@@ -16,47 +16,8 @@ in vec2 texCoord;
 
 // Gets the Texture Units from the main function
 uniform sampler2D diffuse0; //texture
-uniform sampler2D specular0;
-//parameters to set w shader.set
-// Gets the color of the light from the main function
-uniform vec3 lightColor;
-// Gets the position of the light from the main function
-uniform vec3 lightPosition;
-// Gets the position of the camera from the main function
-uniform vec3 camPos;
-
-vec4 direcLight()
-{
-
-	vec3 lightVec = lightPosition - vec3(0); // lightDirection sun
-
-	// ambient lighting
-	float ambient = 0.25f;
-
-	// intensity of light with respect to distance
-	// float dist = length(lightVec);
-	// float a = 3.0;
-	// float b = 0.7;
-	// float inten = 1.0f / (a * dist * dist + b * dist + 1.0f);
-
-	// diffuse lighting
-	vec3 normal = normalize(Normal);
-	vec3 lightDirection = normalize(lightVec);
-	float diffuse = max(dot(normal, lightDirection), 0.0f);
-
-	// specular lighting
-	float specularLight = 0.50f;
-	vec3 viewDirection = normalize(camPos - crntPos);
-	vec3 reflectionDirection = reflect(-lightDirection, normal);
-	float specAmount = pow(max(dot(viewDirection, reflectionDirection), 0.0f), 16);
-	float specular = specAmount * specularLight;
-
-
-	return (texture(diffuse0, texCoord));
-	// return (texture(diffuse0, texCoord) * (diffuse + ambient) + texture(specular0, texCoord).r * specular * inten) * lightColor;
-}
 
 void main() {
 	// outputs final color
-	FragColor = direcLight();
+	FragColor = texture(diffuse0, texCoord);
 }

126c1c91c2bbe327224d29552c64915942181202
Greg0s <gregoire.tinnes@outlook.fr>
2023-06-02 16:37:17 +0200

    update setting clean code

--- Project/setting/setting.cpp
+++ Project/setting/setting.cpp
@@ -1,74 +1,82 @@
 #include "setting.hpp"
+#include "settingParams.hpp"
 
-Setting::Setting(std::string fileGLTF)
+Setting::Setting(const std::string& fileGLTF)
     : m_planet(fileGLTF), m_rot(0.f) {}
 
 void Setting::rotate()
 {
-    m_rot += 0.01;
+    const float rotUnit = p6::PI / 800;
+    m_rot += rotUnit;
+}
+
+void Setting::draw(const p6::Shader& shader, const settingParams& params)
+{
+    glm::mat4 base = glm::mat4(1.f);
+
+    base = glm::translate(base, {params.pos.x, params.pos.y, params.pos.z});
+    base = glm::rotate(base, m_rot, glm::vec3(0.0f, 1.0f, 0.f));
+    base = glm::rotate(base, params.incl, params.inclAxis);
+    base = glm::scale(base, params.scale);
+
+    m_planet.draw(shader, base);
 }
 
 void Setting::drawSetting(const p6::Shader& shader)
 {
-    glm::mat4   base    = glm::mat4(1.f);
-    const float rotUnit = p6::PI / 800;
-
-    m_rot += rotUnit;
+    rotate();
 
     // Planet 1
-    base = glm::translate(base, glm::vec3(3, -0.3, 0.2));
-    base = glm::rotate(base, m_rot, glm::vec3(0.0f, 1.0f, 0.f));
-    base = glm::rotate(base, p6::PI / 6, glm::vec3(1.0f, 0.0f, 0.f));
-    base = glm::scale(base, glm::vec3(0.1));
+    settingParams params1 = {
+        glm::vec3(3, -0.3, 0.2),
+        p6::PI / 6,
+        glm::vec3(1.0f, 0.0f, 0.f),
+        glm::vec3(0.1)};
 
-    m_planet.draw(shader, base);
-
-    base = glm::mat4(1.f);
+    draw(shader, params1);
 
     // Planet 2
-    base = glm::translate(base, glm::vec3(0, -0.2, 2));
-    base = glm::rotate(base, m_rot, glm::vec3(0.0f, 1.0f, 0.f));
-    base = glm::rotate(base, p6::PI / 12, glm::vec3(1.0f, 0.0f, 0.f));
-    base = glm::scale(base, glm::vec3(0.025));
+    settingParams params2 = {
+        glm::vec3(0, -0.2, 2),
+        p6::PI / 12,
+        glm::vec3(0.0f, 1.0f, 0.f),
+        glm::vec3(0.025)};
 
-    m_planet.draw(shader, base);
-
-    base = glm::mat4(1.f);
+    draw(shader, params2);
 
     // Planet 3
-    base = glm::translate(base, glm::vec3(-1, 0.5, 3.5));
-    base = glm::rotate(base, m_rot, glm::vec3(0.0f, 1.0f, 0.f));
-    base = glm::scale(base, glm::vec3(0.2));
+    settingParams params3 = {
+        glm::vec3(-1, 0.5, 3.5),
+        0,
+        glm::vec3(0.0f, 1.0f, 0.f),
+        glm::vec3(0.2)};
 
-    m_planet.draw(shader, base);
-
-    base = glm::mat4(1.f);
+    draw(shader, params3);
 
     // Planet 4
-    base = glm::translate(base, glm::vec3(0.5, 0., -1.5));
-    base = glm::rotate(base, m_rot, glm::vec3(0.0f, 1.0f, 0.f));
-    base = glm::rotate(base, -p6::PI / 7, glm::vec3(0.0f, 0.0f, 1.f));
-    base = glm::scale(base, glm::vec3(0.05));
+    settingParams params4 = {
+        glm::vec3(0.5, 0., -1.5),
+        -p6::PI / 7,
+        glm::vec3(0.0f, 0.0f, 1.f),
+        glm::vec3(0.05)};
 
-    m_planet.draw(shader, base);
-
-    base = glm::mat4(1.f);
+    draw(shader, params4);
 
     // Planet 5
-    base = glm::translate(base, glm::vec3(1.5, 0.6, -4));
-    base = glm::rotate(base, m_rot, glm::vec3(0.0f, 1.0f, 0.f));
-    base = glm::rotate(base, -p6::PI / 10, glm::vec3(1.0f, 0.0f, 0.f));
-    base = glm::scale(base, glm::vec3(0.15));
+    settingParams params5 = {
+        glm::vec3(1.5, 0.6, -4),
+        -p6::PI / 10,
+        glm::vec3(1.0f, 0.0f, 0.f),
+        glm::vec3(0.15)};
 
-    m_planet.draw(shader, base);
-
-    base = glm::mat4(1.f);
+    draw(shader, params5);
 
     // Planet 6
-    base = glm::translate(base, glm::vec3(-3, 0.6, -1));
-    base = glm::rotate(base, m_rot, glm::vec3(0.0f, 1.0f, 0.f));
-    base = glm::rotate(base, -p6::PI / 10, glm::vec3(1.0f, 0.0f, 0.f));
-    base = glm::scale(base, glm::vec3(0.15));
+    settingParams params6 = {
+        glm::vec3(-3, 0.6, -1),
+        -p6::PI / 10,
+        glm::vec3(1.0f, 0.0f, 0.f),
+        glm::vec3(0.15)};
 
-    m_planet.draw(shader, base);
+    draw(shader, params6);
 }
--- Project/setting/setting.hpp
+++ Project/setting/setting.hpp
@@ -2,15 +2,18 @@
 
 #include "../loaderGLTF/gltf.hpp"
 #include "p6/p6.h"
+#include "settingParams.hpp"
 
 class Setting {
 private:
-    Gltf  m_planet;
+    Gltf m_planet;
+
     float m_rot;
 
 public:
-    explicit Setting(std::string fileGLTF);
+    Setting(const std::string& fileGLTF);
 
     void rotate();
+    void draw(const p6::Shader& shader, const settingParams& params);
     void drawSetting(const p6::Shader& shader);
 };
